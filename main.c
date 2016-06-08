6d5fd1d3da237110593d648214b33b64a997fb0c
Ryuichiroh <an94_49@yahoo.co.jp>
2016-06-08 05:52:43 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(int *a, int *b)
+{
+    return *a - *b;
+}
+
+int main()
+{
+   int n;
+   printf("n: ");
+   scanf("%d", &n);
+   
+   int i, x[n];
+   for (i = 0; i < n; i++)
+   {
+      printf("x[%d]: ", i);
+      scanf("%d", &x[i]);
+   }
+   qsort(x, n, sizeof(int), (int(*)(const void*, const void*))cmp);
+   
+   if (n % 2 == 0)
+   {
+      printf("median: %0.6f", (double)(x[n / 2 - 1] + x[n / 2]) / 2);
+   }
+   else
+   {
+      printf("median: %0.6f", (double)x[n / 2]);
+   }
+   return 0;
+}
