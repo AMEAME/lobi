ca5ab2307735ea96fab508c5d08595fc8ca0967b
Ryuichiroh <an94_49@yahoo.co.jp>
2016-06-14 09:24:25 -0700

    a

--- test_thread.py
+++ test_thread.py
@@ -1,23 +1,22 @@
+from __future__ import print_function  # python2と3の互換のため
+from threading import Thread  # threadingモジュールからThreadクラスを読み込み
+import six  # これもpython2と3の互換のため
 
-from __future__ import print_function
-from threading import Thread
-import six
 
 class TestThread(Thread):
- 
     def __init__(self):
         Thread.__init__(self)
         self.stop_requested = False
         self.data = 0
- 
+
     def run(self):
+        # この変数がFalseのときdataをインクリメントします。
         while not stop_requested:
             self.data += 1
-            
- 
-if __name__ == "__main__":
+
+if __name__ == "__main__":  # <- __name__ == "__main__"を知らない場合は調べてね
     th = TestThread()
-    th.start()
-    six.moves.input()
-    th.stop_requested = True
-    print(th.data)
+    th.start()  # この関数がrunメソッドを呼び出します。
+    six.moves.input()  # python2の場合はrow_input関数になり、3の場合はinput関数になります。
+    th.stop_requested = True  # この変数をTrueにすることによってインクリメントを止めます。
+    print(th.data)  # dataを表示
