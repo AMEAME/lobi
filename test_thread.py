100d2fbafa54d224c1f827c9e1dc960ce2a6725e
ike <ike@ike-no-MacBook-Air.local>
2016-06-14 22:21:19 +0900

    a

--- test_thread.py
+++ test_thread.py
@@ -3,12 +3,11 @@ from __future__ import print_function
 from threading import Thread
 import six
 
-stop_requested = False
-
 class TestThread(Thread):
  
     def __init__(self):
         Thread.__init__(self)
+        self.stop_requested = False
         self.data = 0
  
     def run(self):
@@ -20,5 +19,5 @@ if __name__ == "__main__":
     th = TestThread()
     th.start()
     six.moves.input()
-    stop_requested = True
+    th.stop_requested = True
     print(th.data)
