bba99837a1fb09fd32f5f8680f4fc0aaf53e4b40
ike <ike@ike-no-MacBook-Air.local>
2016-06-17 01:02:16 +0900

    simplify equation.c

--- equation.c
+++ equation.c
@@ -1,63 +1,5 @@
 #include <stdio.h>
 
-typedef struct Term
-{
-    char *variable;
-    int coefficient;
-    int degree;
-} Term;
-
-void differentiate(Term *terms, int n)
-{
-    int i;
-    for (i = n; 0 <= i; i--)
-    {
-        terms[i].coefficient *= terms[i].degree--;
-    }
-}
-
-void display(Term *terms, int n)
-{
-    int i;
-    for (i = n; 0 <= i; i--)
-    {
-        if (terms[i].coefficient != 0)
-        {
-            if (i != n)
-            {
-                printf(" + ");
-            }
-            if (terms[i].degree <= 0)
-            {
-                printf("%d", terms[i].coefficient);
-            }
-            else if (terms[i].degree == 1)
-            {
-                if (terms[i].coefficient == 1)
-                {
-                    printf("%s", terms[i].variable);
-                }
-                else
-                {
-                    printf("%d%s", terms[i].coefficient, terms[i].variable);
-                }
-            }
-            else
-            {
-                if (terms[i].coefficient == 1)
-                {
-                    printf("%s^%d",terms[i].variable, terms[i].degree);
-                }
-                else
-                {
-                    printf("%d%s^%d", terms[i].coefficient, terms[i].variable, terms[i].degree);
-                }
-            }
-        }
-    }
-    printf("\n");
-}
-
 int main()
 {
     // -- nを入力させます。--
@@ -69,12 +11,17 @@ int main()
     int a[n + 1];
     int i;
     
+    // この下のfor文の書き方は覚えておいてください。
+    // for (int i = 0; i < n; i++) {}
+    // これで0からn-1までn回ループさせるという意味です。
     for (i = 0; i < n + 1; i++)
     {
-        int index = n - i;
+        int index = n - i;  // 後ろからループさせるためにn-iを使う
+        
         int coefficient;
         printf("a%d = ", index);
         scanf("%d", &coefficient);
+        
         a[index] = coefficient;
     }
     
@@ -84,15 +31,16 @@ int main()
     for (i = 0; i < n + 1; i++)
     {
         int index = n - i;
-        if (a[index] == 0) continue;
-        if (i != 0) printf(" + ");
-        if (a[index] != 1) printf("%d", a[index]);
-        if (index > 0) printf("x");
-        if (index < -1 || 1 < index) printf("^%d", index);
+        if (a[index] == 0) continue;  // 係数がない場合は、次の項に行きます
+        
+        if (i != 0) printf(" + ");  // 初項以外に+を書きます
+        if (a[index] != 1) printf("%d", a[index]);  // 係数が1じゃないとき係数を出力します
+        if (index > 0) printf("x");  // 次数が0じゃないとき、"x"を出力します
+        if (1 < index) printf("^%d", index);  // 次数が2以上のとき、"^"と次数を出力します
     }
     printf("\n");
     
-    // -- f(x)を表示
+    // -- f'(x)を表示
     printf("f'(x) = ");
     
     for (i = 0; i < n + 1; i++)
@@ -102,7 +50,7 @@ int main()
         if (i != 0) printf(" + ");
         if (a[index] * index != 1) printf("%d", a[index] * index);
         if (index > 1) printf("x");
-        if (index < 0 || 2 < index) printf("^%d", index - 1);
+        if (2 < index) printf("^%d", index - 1);
     }
     printf("\n");
     
