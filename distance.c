6b9aaba1854f5198a89164866409a71a17138fdc
ike <ike@ike-no-MacBook-Air.local>
2016-06-23 17:41:16 +0900

    add distance.c

---
+++ distance.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int distance(int a, int b, int c, int p, int q)
+{
+    if (!(a || b)) return -1;
+    return abs(a * p + b * q + c) / sqrt(a * a + b * b);
+}
+
+int main()
+{
+    int a, b, c, p, q, d;
+    
+    printf("a: ");
+    scanf("%d", &a);
+    
+    printf("b: ");
+    scanf("%d", &b);
+    
+    printf("c: ");
+    scanf("%d", &c);
+    
+    printf("p: ");
+    scanf("%d", &p);
+    
+    printf("q: ");
+    scanf("%d", &q);
+    
+    d = distance(a, b, c, p, q);
+    
+    printf(d > 0 ? "d: %d\n" : "error\n", d);
+    return 0;
+}
